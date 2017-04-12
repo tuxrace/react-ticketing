1cc184941dd6af0a0bc4b82a75be769bee445150
Gerard Banasig <tuxrace@yahoo.com>
2017-04-12 19:25:25 +0800

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-  
+  <div>Hello</div>
 }
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    entry: [
-        {add: './src/index.js'},
-        {vendor: [react, react-dom]},
-    ],
-    output: {filename: __dir +'/'+ [name].build.js},
-    module:{
-        loaders:[
-            {
-                test:/\.js$/,
-                loader:'babel-loader',
-                exclude:/node_modules/
-            }
-        ]
-    }
+  entry: [
+    { add: './src/index.js' },
+    { vendor: ['react', 'react-dom'] },
+  ],
+  output: { filename: __dirname + '/' + [name].build.js },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
